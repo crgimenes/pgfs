793f560f54cde49ad008b350f3823e40779c50cd
crgimenes <crg@crg.eti.br>
2023-06-27 09:12:04 -0300

    fix sql

--- testdb.sql
+++ testdb.sql
@@ -4,8 +4,17 @@ CREATE TABLE clients (
     address text
 );
 
-COPY public.clients (id, name, address) FROM stdin;
-1	Umbrella Corporation	545 S Birdneck RD STE 202B Virginia Beach, VA 23451
-2	OCP Omni Consumer Products	Delta City (formerly Detroit) 
-3	Weyland-Yutani Corporation	Weyland-Yutani Corporation HQ, Tokyo
-\.
+insert into clients (id, name, address) values (1, 'Umbrella Corporation', '123 Main St, Anytown, USA');
+insert into clients (id, name, address) values (2, 'Weyland-Yutani', '123 Main St, Anytown, USA');
+insert into clients (id, name, address) values (3, 'Aperture Science', '123 Main St, Anytown, USA');
+insert into clients (id, name, address) values (4, 'Acme Corporation', '123 Main St, Anytown, USA');
+insert into clients (id, name, address) values (5, 'Tyrell Corporation', '123 Main St, Los Angeles, CA');
+insert into clients (id, name, address) values (6, 'Cyberdyne Systems', '123 Main St, Los Angeles, CA');
+insert into clients (id, name, address) values (7, 'Initech', '123 Main St, Los Angeles, CA');
+insert into clients (id, name, address) values (8, 'Globex Corporation', '123 Main St, Los Angeles, CA');
+insert into clients (id, name, address) values (9, 'Wonka Industries', '123 Main St, Los Angeles, CA');
+insert into clients (id, name, address) values (10, 'Stark Industries', '123 Main St, Los Angeles, CA');
+insert into clients (id, name, address) values (11, 'Wayne Enterprises', '123 Main St, Los Angeles, CA');
+insert into clients (id, name, address) values (12, 'BiffCo', '123 Main St, Los Angeles, CA');
+insert into clients (id, name, address) values (13, 'Gringotts', '123 Main St, Los Angeles, CA');
+insert into clients (id, name, address) values (14, 'Oceanic Airlines', '123 Main St, Los Angeles, CA');
